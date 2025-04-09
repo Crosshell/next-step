cd5a3d2f9493d2c29083c18d5867e4150466e3cf
Crosshell <confighell@gmail.com>
2025-04-09 19:01:52 +0300

    Add user password encryption

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -3,13 +3,21 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma.service';
 import { User } from '@prisma/client';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    return this.prismaService.user.create({ data: createUserDto });
+    const hashedPassword = await argon2.hash(createUserDto.password);
+
+    return this.prismaService.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+      },
+    });
   }
 
   async findAll(): Promise<User[]> {
@@ -17,29 +25,40 @@ export class UserService {
   }
 
   async findById(id: string): Promise<User | null> {
-    return this.prismaService.user.findUnique({ where: { id } });
+    return this.prismaService.user.findUnique({
+      where: { id },
+    });
   }
 
   async findByEmail(email: string): Promise<User | null> {
-    return this.prismaService.user.findUnique({ where: { email } });
+    return this.prismaService.user.findUnique({
+      where: { email },
+    });
   }
 
   async update(id: string, updateUserDto: UpdateUserDto): Promise<User | null> {
-    const user = await this.prismaService.user.findUnique({ where: { id } });
-
+    const user = await this.prismaService.user.findUnique({
+      where: { id },
+    });
     if (!user) return null;
+
+    const data = { ...updateUserDto };
+    if (data.password) {
+      data.password = await argon2.hash(data.password);
+    }
 
     return this.prismaService.user.update({
       where: { id },
-      data: updateUserDto,
+      data,
     });
   }
 
   async remove(id: string): Promise<User | null> {
     const user = await this.prismaService.user.findUnique({ where: { id } });
-
     if (!user) return null;
 
-    return this.prismaService.user.delete({ where: { id } });
+    return this.prismaService.user.delete({
+      where: { id },
+    });
   }
 }
