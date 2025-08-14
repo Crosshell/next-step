f9a1e65f27f8ae535bc4e9f5974666b31163f187
kytaec <vladislavtokovenko4@gmail.com>
2025-08-14 09:56:21 +0300

    feat: add styles for SideBox

--- apps/front/src/components/Vacancy/SideBox.tsx
+++ apps/front/src/components/Vacancy/SideBox.tsx
@@ -1,20 +1,39 @@
 'use client';
-
-import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 
 import SideBoxButton from './SideBoxButton';
+import HoveredItem from '../HoveredItem/HoveredItem';
 
 import classes from './SideBox.module.css';
 
 import { VacancySideBoxData } from '@/types/vacancy';
+import { capitalize, toKebabCase } from '@/utils/convertData';
+import { validateImageUrl } from '@/utils/validation';
 
 interface Props {
   data: VacancySideBoxData;
 }
 
 export default function SideBox({ data }: Props) {
+  const [logoUrl, setLogoUrl] = useState('/images/suitcase.png');
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const companyLogo = data.companyLogo ?? '';
+    setIsLoaded(false);
+
+    validateImageUrl(companyLogo).then((isValid) => {
+      if (isValid) {
+        setLogoUrl(companyLogo);
+      } else {
+        setLogoUrl('/images/organization-dark.png');
+      }
+      setIsLoaded(true);
+    });
+  }, [data.companyLogo]);
+
   return (
     <motion.div
       className={classes['side-box']}
@@ -22,26 +41,33 @@ export default function SideBox({ data }: Props) {
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <Image
-        src="/images/organization-dark.png"
+      <img
+        src={logoUrl}
         alt="stairs-image"
         width={100}
         height={100}
-        priority
+        style={{
+          opacity: isLoaded ? 1 : 0,
+          transition: 'opacity 0.3s ease-in-out',
+        }}
       />
       <h3>{data?.companyName}</h3>
       <h4 className={classes['site-link']}>
-        <motion.div whileHover={{ scale: 1.1 }}>
-          <Link href={data?.companyUrl || '/'}>Visit Website</Link>
-        </motion.div>
+        <Link href={data?.companyUrl || '/'}>
+          <HoveredItem>Visit Website</HoveredItem>
+        </Link>
       </h4>
       <section>
         <h4>Employment Type:</h4>
-        <p>{data?.employmentType}</p>
+        <p>
+          {capitalize(
+            data?.employmentType.map((v: string) => toKebabCase(v)).join(', ')
+          )}
+        </p>
       </section>
       <section>
         <h4>Work Format:</h4>
-        <p>{data?.workFormat}</p>
+        <p>{capitalize(data?.workFormat.join(', '))}</p>
       </section>
       <section>
         <h4>Office Location:</h4>
